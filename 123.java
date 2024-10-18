a0252eaff8f127b76df8b4ea299e28d248603418
darsanna <ri.darsanna@gmail.com>
2024-10-18 17:28:16 +0300

    2.1

--- 123.java
+++ 123.java
@@ -1,14 +1,12 @@
 import java.util.Scanner; 
 public class DivisibleByFive { 
         public static void main(String[] args) { 
-           Scanner scanner = new Scanner(System.in); 
-              // Запрашиваем у пользователя начальное и конечное числа 
+           Scanner scanner = new Scanner(System.in);  
              System.out.print("Введите начальное число: ");
              int start = scanner.nextInt(); 
 
             System.out.print("Введите конечное число: "); 
             int end = scanner.nextInt(); 
-           // Выводим числа, которые делятся на 5 
            System.out.println("Числа от " + start + " до " + end + ", которые делятся на 5:");
           for (int number = start; number <= end; number++) {
                if (number % 5 == 0) { 
